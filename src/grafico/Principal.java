604414ac40a01c040b78629470bd2a60dbc4590e
sami9 <samiprlt@hotmail.com>
2014-05-21 13:17:22 -0600

    _Clase Principal.

---
+++ src/grafico/Principal.java
@@ -0,0 +1,48 @@
+package grafico;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JRadioButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+
+import java.awt.Font;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.ImageIcon;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.SwingConstants;
+import javax.swing.JList;
+import javax.swing.ButtonGroup;
+
+public class Principal extends JFrame{
+	
+	public static void main(String[]args){
+		
+		
+		
+		
+	}
+
+	public Principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
+
+
